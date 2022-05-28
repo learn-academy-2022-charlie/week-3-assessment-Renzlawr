9dc43cf4929a9a12088c71ec394ab31061441c2f
Renzlawr <RSLogan23@gmail.com>
2022-05-28 17:01:48 -0600

    Completed: I brain stop worky think

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,42 +15,200 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+// Create a test wtih describe that takes in fibSequence as a function
+// It takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
+// expects two results
 
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+describe("fibSequence", () => {
+    it("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.", () => {
+        const fibLength1 = 6
+        // Expected output: [1, 1, 2, 3, 5, 8]
+        const fibLength2 = 10
+        // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+        expect(fibSequence(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+        expect(fibSequence(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    })
+}) 
 
+// Red
+// FAIL  ./code-challenges.test.js
+// fibSequence
+//   ✕ takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence. (1 ms)
+// ● fibSequence › takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
+//   ReferenceError: fibSequence is not defined
 
 // b) Create the function that makes the test pass.
 
+// Create a function named fibSequence that takes in length as a parameter
+// Create variables, result will be our array starting with our two starting nums, num1 with be the first number in our array, num2 will be the second number in our array, next will be the next number.
+// for loop with i set to 2(since our result starts with 2 values and we don't need to do anything to either) and as long as i is less than our length param we'll do a lot of confusing reassigning
+// next will be the sum of num1 and num2
+// num1 will become num2
+// num2 will become next 
+// we want to push next into our result array
+// we also want to update count so our iteration continues until our conditon is met
+// pass test
+
+const fibSequence = (length) => {
+    let result = [1, 1],
+    num1 = result[0],
+    num2 = result[1],
+    next
+
+    for(let i = 2; i < length; i++) {
+        next = num1 + num2
+        num1 = num2
+        num2 = next
+        result.push(next)
+    }
+    return result
+}
+// This question was extremely difficult for me for some reason, but giving up is not an option for me! I had to look at some outside examples and the formula for the Fibonacci sequence. I also learned I can create multiple variables with one variable declaration and some commas which is cool. 
+
+// Green 
+// PASS  ./code-challenges.test.js
+//   fibSequence
+//     ✓ takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence. (4 ms)
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-// Expected output: [-9, 7, 9, 199]
+// Create a test wtih describe that takes in sortOddsUp as a function
+// takes in an array and returns a new array of only odd numbers sorted from least to greatest.
+// expects two results
 
-const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
-// Expected output: [-823, 7, 23]
+describe("sortOddsUpFinalRefactor", () => {
+    it("takes in an array and returns a new array of only odd numbers sorted from least to greatest.", () => {
+        const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+        // Expected output: [-9, 7, 9, 199]
+        const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+        // Expected output: [-823, 7, 23]
+        expect(sortOddsUpFinalRefactor(fullArr1)).toEqual([-9, 7, 9, 199])
+        expect(sortOddsUpFinalRefactor(fullArr2)).toEqual([-823, 7, 23])
+    })
+})
 
+// Red
+// FAIL  ./code-challenges.test.js
+// sortOddsUp
+//   ✕ takes in an array and returns a new array of only odd numbers sorted from least to greatest. (1 ms)
+// ● sortOddsUp › takes in an array and returns a new array of only odd numbers sorted from least to greatest.
+//   ReferenceError: sortOddsUp is not defined
 
 // b) Create the function that makes the test pass.
 
+// Create a function sortOddsUp that takes in array as its parameter
+// create a new variable called numArr and filter out everything that isn't a number
+// create a new variable called odds and filter out all even numbers
+// return the odds array sorted in ascending order 
+// pass test
+
+const sortOddsUp = (array) => {
+    let numArr = array.filter(value => {
+        return typeof value === "number"
+    })
+    let odds = numArr.filter(num => {
+        return num % 2 != 0
+    })
+    return odds.sort((a, b) => a - b)
+}
+
+// Green 
+// PASS  ./code-challenges.test.js
+// sortOddsUp
+//   ✓ takes in an array and returns a new array of only odd numbers sorted from least to greatest. (2 ms)
+
+// Refactor 1 
+
+const sortOddsUpRefactor = (array) => {
+    return array.filter(value => {
+        return typeof value === "number"
+    }).filter(num => {
+        return num % 2 != 0
+    }).sort((a, b) => a - b)
+}
+
+// Final Refactor
+
+const sortOddsUpFinalRefactor = (array) => {
+    return array.sort((a, b) => a - b).filter(value => {
+        return typeof value === "number" && value % 2 != 0
+    })
+}
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
+// Create a test wtih describe that takes in addSum as a function
+// takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+// expects three results
 
-const numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
+describe("addSum", () => {
+    it("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.", () => {
+        const numbersToAdd1 = [2, 4, 45, 9]
+        // Excpected output: [2, 6, 51, 60]
+        const numbersToAdd2 = [0, 7, -8, 12]
+        // Expected output: [0, 7, -1, 11]
+        const numbersToAdd3 = []
+        // Expected output: []
+        expect(addSum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+        expect(addSum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+        expect(addSum(numbersToAdd3)).toEqual([])
+    })
+})
 
-const numbersToAdd3 = []
-// Expected output: []
-
+// Red 
+// FAIL  ./code-challenges.test.js
+// addSum
+//   ✕ takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+// ● addSum › takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+//   ReferenceError: addSum is not defined
 
 // b) Create the function that makes the test pass.
+
+// Create a function named addSum that takes in array as its param
+// map through our array taking in value and index as params of the callback func
+// slice the array between 0 and the next index
+// reduce that to obtain the sums
+// Pass test
+
+const addSum = (array) => {
+    return array.map((value, index) => {
+        return array.slice(0, index + 1).reduce(
+            (prev, curr) => prev + curr)
+    })
+}
+
+// This question again was extremely difficult. Reduce was challenging to find a solution for getting each sum instead of the total sum. Is there better ways to think about this problem?
+
+// Green!!!!!
+// PASS  ./code-challenges.test.js
+// addSum
+//   ✓ takes in an array and returns an array of the accumulating sum. An empty array should return an empty array. (1 ms)
+
+
+
+// A few of my attemps of fragmented code that ended up being wrong - A lot more were deleted
+
+// const addSum = (array) => {
+//     let newArr = [array[0]]
+//     for(let i = 0; i <= array.length; i++) {
+//      let sum = array.at(+1) + array.at(-1)
+//      newArr.push(array[i] + array[nextNum])
+//         let num = array.at[i]
+//         return num
+//     }
+// }
+        
+// const addSum = (array) => {
+//     let newArr = []
+//     return array.reduce(
+//         (previousValue, currentValue) => newArr.push(previousValue + currentValue)
+//       )     
+// }
+
+
+
+
--- interview-questions.md
+++ interview-questions.md
@@ -9,59 +9,59 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: This is this which is obviously this so that this can take in this with this hehehe. `this` is a really useful and fun tool that we use every single day in react! Since everything we're doing in react is class based we need a way to refer to certain data within it. Say we want to call a method we made, we would have to refer to it as `this.methodName` since react wouldn't be able to understand what our method is before it's initialized. One of the most important ways we use `this` is to get data in the state with dot notation since state is an object. It would look something like this `{this.state.name}`. 
 
-  Researched answer:
+  Researched answer: `this` is a keyword that references JavaScript elements within the scope of its use. I found out that you can use `this` in your browser console to see the global Window object. `this` is also very important for passing props and refering to them within components.
 
 
 
 2. What is React? Why would you use it?
 
-  Your answer:
+  Your answer: React is a JavaScript Library that is built around user interaction. We use it so we can have real time updates based on users input that are efficient and don't need to re render the entire page. Instead we have the Virtual DOM(Virtual Document Object Model) which takes nodes and updates them one at a time based on changes instead of all at once. The Virtual DOM updates the DOM directly. Even apps like facebook and instagram are made using React Native!
 
-  Researched answer:
+  Researched answer: I found out some interesting things about react, one being the Shadow DOM which some people misinterpret as being the same as the Virtual DOM. The Shadow DOM is a technology designed for scoping variables and CSS within web components in your browser. Another being React Fiber which seems to be a complete overhaul of the old React reconciler(which is how we get efficient UI updates) and it tries to promote incremental rendering of the Virtual DOM. 
 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+  Your answer: The lifecycle method that is required in a React class component is `render()`. We use render literally to render the HTML elements on our page. We usually put our return inside of the render so that we can return and render all the code that we've written. Lifecycle methods are used to update the DOM at specific times within the lifecycle. 
 
-  Researched answer:
+  Researched answer: Render is the only method that is absolutely necessary in a class component and takes the role of describing the view that will be rendered in your browser. There are however other lifecycle methods that can be used along with return such as `componentWillMount()`, `constructor()`, `componentDidMount()` for mounting, and a lot more for rendering and unmounting.
 
 
 
 4. What is JSX? What is one syntax difference between HTML and JSX?
 
-  Your answer:
+  Your answer: JSX is a syntax extension that is used for javascript so that we might include HTML within our JavaScript and have them work together. This is very important and is a big part of what makes React. One syntax difference off the top of my head would be the way we create containers and how JSX introduces react fragments which are just empty tags that create a unnamed container allowing us to have cleaner code.
 
-  Researched answer:
+  Researched answer: JSX allows us to produce React elements and combine JavaScript with HTML so we don't have to make multiple files and link them together. We can just do it all in one! Some other syntax differences I found were pretty cool, first JSX allows all tags to be self closing if you so choose. Making a class for an element isn't defined as the attribute class, but as className. You can also write inline styling as objects using JSX.
 
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: Yarn is a very powerful tool that makes it all possible to easily create react apps! Yarn is a package manager that bundles all the node modules and react dependencies together so that all we have to do is run the commands `$ yarn create react-app app-name`. We've been using it everyday at LEARN for our React challenges. The files that are modified in a react app when you run `$ yarn` are allll of those node modules that came with your react app are updated based on what your yarn.lock file tells it. You run the yarn command after pulling down a react app from github so that you can download the node modules and actually start your server for the app. Node modules are not pushed up to github, and if they are you mayyyy wanna check your gitignore file!
 
-  Researched answer:
+  Researched answer: Yarn was originally developed as a package manager for the JavaScript runtime environment Node.js by Meta. You don't have to use yarn as your package manager if you don't want to, the main alternative is npm and the updated version npx. 
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: An anonymous function is a function that has no name! We recently figured out in class that it can be used to stop a method in React that was passed as a prop in an onClick from automatically calling itself. I'm sure there's a lot more uses for these that can be pretty important. 
 
-  Researched answer:
+  Researched answer: I found out anonymous functions are usually not accessible after creation and can be used inside other functions. We also would use them a lot for event handlers.
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: Conditional rendering is pretty similar to how we use conditionals normally. The main difference is it only renders certain items based on if the condition is met
 
-2. Object-oriented programming:
+2. Object-oriented programming: Also known as OOP is a way that we code using classes and objects. We could also use function oriented programming instead.
 
-3. Ruby:
+3. Ruby: Ruby is a scripting language that happens to have a very similar syntax to python. Github is made with ruby too!
 
-4. Ruby blocks:
+4. Ruby blocks: These are anonymous functions! They can be passed into methods and have arguments
 
-5. Ruby hashes:
+5. Ruby hashes: Similar to an object? They house key value pairs 
